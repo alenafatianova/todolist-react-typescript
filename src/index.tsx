e44a639dcf64b9244be9ab156840fcd260c66010
Alena Fatianova <ravemybody@gmail.com>
2020-11-24 17:24:06 +0500

    wrap AppWithRedux with Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { AppWithRedux } from './state/AppWithRedux';
+import {Provider} from 'react-redux'
+import { store } from './state/store';
 
-ReactDOM.render(<AppWithRedux />,  document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <AppWithRedux />
+</Provider>,  
+
+document.getElementById('root'));
 
 
 serviceWorker.unregister();
