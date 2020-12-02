9b94f8acd6bdc70608e47dbfd08c61823b65f6fb
Alena Fatianova <ravemybody@gmail.com>
2020-12-02 13:40:37 +0500

    stories action for AddItemForm

--- src/components/TodoList/AddItemForm/AdddItemForm.stories.tsx
+++ src/components/TodoList/AddItemForm/AdddItemForm.stories.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { AddItemForm } from './AddItemForm'
+import {action} from '@storybook/addon-actions'
 
 export default {
     title: 'AddItem Form',
     component: AddItemForm,
 } as Meta;
 
+
+const actionCallback = action('item form was clicked with this value:')
+
 export const AddItemFormStories: Story<AddItemFormProps> = (props: any) => {
-   return  <AddItemForm/>
+   return  <AddItemForm addItem={actionCallback}/>
 }
