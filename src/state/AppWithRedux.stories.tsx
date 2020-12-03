4ab0479a7608b1e75bc0ff04dbe5ec64f36cf538
Alena Fatianova <ravemybody@gmail.com>
2020-12-03 18:05:46 +0500

    add decorator: ReduxStoriesDecorator

--- src/state/AppWithRedux.stories.tsx
+++ src/state/AppWithRedux.stories.tsx
@@ -3,16 +3,16 @@ import { Meta } from '@storybook/react/types-6-0'
 import AppWithRedux from './AppWithRedux';
 import {Provider} from 'react-redux'
 import {store} from '../state/store' 
+import { ReduxStoreProviderDecorator } from '../stories/ReduxStoriesProviderDecorator';
 
 export default {
     title: 'App With Redux',
-    component: AppWithRedux
+    component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
 } as Meta;
 
 export const AppWithReduxtSories = () => {
     return <> 
-        <Provider store={store}>
-            <AppWithRedux/>
-        </Provider>
+        <AppWithRedux/>
     </>
 }
